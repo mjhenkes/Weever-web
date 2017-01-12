49c83149665e369de4681a7fcc34fe5783f8b731
Matt Henkes <mjhenkes@gmail.com>
2017-01-11 22:45:24 -0600

    trying out the yo node generator

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import weeverWeb from '../lib';
+
+describe('weever-web', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
