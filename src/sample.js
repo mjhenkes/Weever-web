2587d1c3532f04d942565f81cffdd39103c439a3
Matt Henkes <mjhenkes@gmail.com>
2017-01-16 22:16:04 -0600

    adding sample

---
+++ src/sample.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+
+var zerorpc = require("zerorpc");
+
+var client = new zerorpc.Client();
+client.connect("tcp://127.0.0.1:4242");
+
+for (var i = 10; i >= 0; i--) {
+	client.invoke("hello", "RPC", function(error, res, more) {
+    	console.log(res);
+	});
+}
