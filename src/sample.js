2e58fcb1d35f3b182cd45ef0cf79099a395e1328
Matt Henkes <mjhenkes@gmail.com>
2017-01-17 00:10:13 -0600

    a real sample

--- src/sample.js
+++ src/sample.js
@@ -6,7 +6,8 @@ var client = new zerorpc.Client();
 client.connect("tcp://127.0.0.1:4242");
 
 for (var i = 10; i >= 0; i--) {
-	client.invoke("hello", "RPC", function(error, res, more) {
-    	console.log(res);
-	});
+	client.invoke("hello", 0 + (i * 10))
+	// client.invoke("hello", "RPC", function(error, res, more) {
+ //    	console.log(res);
+	// });
 }
