2ae81f588635f952ceca3cb1fe06bcb447f63265
Matt Henkes <mjhenkes@gmail.com>
2017-01-17 00:11:20 -0600

    use the right service name

--- src/sample.js
+++ src/sample.js
@@ -6,7 +6,7 @@ var client = new zerorpc.Client();
 client.connect("tcp://127.0.0.1:4242");
 
 for (var i = 10; i >= 0; i--) {
-	client.invoke("hello", 0 + (i * 10))
+	client.invoke("steer", 0 + (i * 10))
 	// client.invoke("hello", "RPC", function(error, res, more) {
  //    	console.log(res);
 	// });
